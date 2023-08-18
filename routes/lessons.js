129548d87498b12f08e90f3109048361678f2603
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-08-19 02:29:46 +0500

    Index page

--- routes/lessons.js
+++ routes/lessons.js
@@ -14,6 +14,7 @@ router.get('/community', (req, res) => {
     res.render('community', {
         title: "Community",
         isCommunity: true,
+        isRegistred: true,
     })
 
 })
@@ -24,12 +25,14 @@ router.get('/learn', (req, res) => {
         isLearn: true,
         isComplatedModule: true,
         isCurrentModule: false,
+        isRegistred: true,
     })
 })
 router.get('/review', (req, res) => {
     res.render('review', {
         title: "Review",
         isReview: true,
+        isRegistred: true,
     })
 })
 
@@ -37,6 +40,7 @@ router.get('/courses', (req, res) => {
     res.render('courses', {
         title: "Courses",
         isCourses: true,
+        isRegistred: true,
     })
 })
 
