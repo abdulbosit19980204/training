45a396b42ebe3d387a74763c348ccff2a9630e4f
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-08-19 05:00:43 +0500

    Login and Register

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(LessonsRoutes)
 app.use(AuthRoutes)
 
--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,21 @@ router.get('/login', (req, res) => {
         title: "Login"
     })
 })
+router.get('/apply', (req, res) => {
+        res.render('apply', {
+            title: "Apply",
+        })
 
+    })
+    // Posts
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/learn')
+})
+
+router.post('/apply', (req, res) => {
+    console.log(req.body);
+    res.redirect('/login')
+})
 export default router
