d8e84387c813ecb8f5d91c80187422226e35b9ef
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-08-19 05:15:21 +0500

    Static folders

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'hbs')
 app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }))
-
+app.use(express.static("public"))
 app.use(LessonsRoutes)
 app.use(AuthRoutes)
 
