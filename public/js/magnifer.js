0df7a5d975e9949334a28a67825a15e16e873be0
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-09-25 06:15:54 +0500

    magnifer mobile

--- public/js/magnifer.js
+++ public/js/magnifer.js
@@ -1,3 +1,67 @@
+// function magnify(imgID, zoom) {
+//     var img, glass, w, h, bw;
+//     img = document.getElementById(imgID);
+
+//     /* Create magnifier glass: */
+//     glass = document.createElement("DIV");
+//     glass.setAttribute("class", "img-magnifier-glass");
+
+//     /* Insert magnifier glass: */
+//     img.parentElement.insertBefore(glass, img);
+
+//     /* Set background properties for the magnifier glass: */
+//     glass.style.backgroundImage = "url('" + img.src + "')";
+//     glass.style.backgroundRepeat = "no-repeat";
+//     glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+//     bw = 3;
+//     w = glass.offsetWidth / 2;
+//     h = glass.offsetHeight / 2;
+
+//     /* Execute a function when someone moves the magnifier glass over the image: */
+//     glass.addEventListener("mousemove", moveMagnifier);
+//     img.addEventListener("mousemove", moveMagnifier);
+
+//     /*and also for touch screens:*/
+//     glass.addEventListener("touchmove", moveMagnifier);
+//     img.addEventListener("touchmove", moveMagnifier);
+
+//     function moveMagnifier(e) {
+//         var pos, x, y;
+//         /* Prevent any other actions that may occur when moving over the image */
+//         e.preventDefault();
+//         /* Get the cursor's x and y positions: */
+//         pos = getCursorPos(e);
+//         x = pos.x;
+//         y = pos.y;
+//         /* Prevent the magnifier glass from being positioned outside the image: */
+//         if (x > img.width - (w / zoom)) { x = img.width - (w / zoom); }
+//         if (x < w / zoom) { x = w / zoom; }
+//         if (y > img.height - (h / zoom)) { y = img.height - (h / zoom); }
+//         if (y < h / zoom) { y = h / zoom; }
+//         /* Set the position of the magnifier glass: */
+//         glass.style.left = (x - w) + "px";
+//         glass.style.top = (y - h) + "px";
+//         /* Display what the magnifier glass "sees": */
+//         glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+//     }
+
+//     function getCursorPos(e) {
+//         var a, x = 0,
+//             y = 0;
+//         e = e || window.event;
+//         /* Get the x and y positions of the image: */
+//         a = img.getBoundingClientRect();
+//         /* Calculate the cursor's x and y coordinates, relative to the image: */
+//         x = e.pageX - a.left;
+//         y = e.pageY - a.top;
+//         /* Consider any page scrolling: */
+//         x = x - window.pageXOffset;
+//         y = y - window.pageYOffset;
+//         return { x: x, y: y };
+//     }
+// }
+
+
 function magnify(imgID, zoom) {
     var img, glass, w, h, bw;
     img = document.getElementById(imgID);
@@ -21,7 +85,7 @@ function magnify(imgID, zoom) {
     glass.addEventListener("mousemove", moveMagnifier);
     img.addEventListener("mousemove", moveMagnifier);
 
-    /*and also for touch screens:*/
+    /* Add touch event listeners for mobile devices: */
     glass.addEventListener("touchmove", moveMagnifier);
     img.addEventListener("touchmove", moveMagnifier);
 
@@ -30,7 +94,7 @@ function magnify(imgID, zoom) {
         /* Prevent any other actions that may occur when moving over the image */
         e.preventDefault();
         /* Get the cursor's x and y positions: */
-        pos = getCursorPos(e);
+        pos = (e.type === "touchmove") ? getTouchPos(e) : getCursorPos(e);
         x = pos.x;
         y = pos.y;
         /* Prevent the magnifier glass from being positioned outside the image: */
@@ -57,6 +121,12 @@ function magnify(imgID, zoom) {
         /* Consider any page scrolling: */
         x = x - window.pageXOffset;
         y = y - window.pageYOffset;
+        return { x: x, y: y };
+    }
+
+    function getTouchPos(e) {
+        var x = e.touches[0].clientX - img.getBoundingClientRect().left;
+        var y = e.touches[0].clientY - img.getBoundingClientRect().top;
         return { x: x, y: y };
     }
 }
