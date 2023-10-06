e720a21ecfdae0f347c87047180e222982663a9a
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-10-07 00:25:19 +0500

    Compete page

--- routes/compete.js
+++ routes/compete.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import User from "../models/User.js";
+import Lesson from "../models/Lesson.js";
 
 
 const router = Router()
@@ -8,12 +9,14 @@ router.get('/compete/:id', async(req, res) => {
     const userData = req.userData
     const userId = req.userId
     const competerId = req.params.id
-    console.log("userID = ", userId, "competerId", competerId);
+        // console.log("userID = ", userId, "competerId", competerId);
     const competerData = await User.findById(competerId).lean()
+    const lessons = await Lesson.find().sort({ lessonPart: 1 }).lean()
     res.render('compete', {
         title: "Compete",
         userData: userData,
         competerData: competerData,
+        lessons: lessons,
     })
 })
 export default router
