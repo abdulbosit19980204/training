e109206f14b55218655c2a5d0e1130ff69bf9a65
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-10-07 08:58:08 +0500

    Compete

--- routes/test.js
+++ routes/test.js
@@ -6,16 +6,17 @@ const router = Router()
 router.get('/test/:id', async(req, res) => {
     const userData = req.userData
     const id = req.params.id
+    const competeTestId = id
     const competeData = await Lesson.findById(id).lean()
     const page = req.query.page || 1;
     const itemsPerPage = 1; // Change this to your desired number of items per page
     const skip = (page - 1) * itemsPerPage;
     const limit = itemsPerPage;
     const byLesson = competeData.lessonTitle
-    const QuizData = await Quiz.find({ byLesson }).sort({ updatedAt: 1 }).skip(skip).limit(1).lean();
+    const QuizData = await Quiz.find({ byLesson }).sort({ updatedAt: 1 }).skip(skip).limit(limit).lean();
     const QuizDataLen = (await Quiz.find({ byLesson })).length;
     const totalItems = await Quiz.countDocuments();
-    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const totalPages = Math.ceil(QuizDataLen / itemsPerPage);
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
     res.render('test', {
         title: "Competing",
@@ -25,6 +26,7 @@ router.get('/test/:id', async(req, res) => {
         quizDataLen: QuizDataLen,
         pages: pages,
         page: page,
+        competeTestId: competeTestId,
     })
 })
 export default router
